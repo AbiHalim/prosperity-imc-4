4b2fdf01d7a8cdca4342b6ae7fe0f2c3a5f60196
AbiHalim <abishai.halim@gmail.com>
2026-04-28 21:35:33 +0800

    round 5 lah

--- scratch/vfx_spread.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data/ROUND_3/prices_round_3_day_0.csv', sep=';')
-vfx = df[df['product'] == 'VELVETFRUIT_EXTRACT']
-print("VFX Mean Spread:", (vfx['ask_price_1'] - vfx['bid_price_1']).mean())
-print("VFX Median Spread:", (vfx['ask_price_1'] - vfx['bid_price_1']).median())
